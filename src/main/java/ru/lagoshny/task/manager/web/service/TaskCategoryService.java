56f79bac411a70f1dfaa20a581c9cd4e9b90d1af
ilya <ilya@lagoshny.ru>
2019-11-12 20:19:28 +0300

    Added task category service to allow delete category with all categories tasks

---
+++ src/main/java/ru/lagoshny/task/manager/web/service/TaskCategoryService.java
@@ -0,0 +1,39 @@
+package ru.lagoshny.task.manager.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.lagoshny.task.manager.domain.entity.Task;
+import ru.lagoshny.task.manager.domain.entity.TaskCategory;
+import ru.lagoshny.task.manager.domain.repository.TaskCategoryRepository;
+import ru.lagoshny.task.manager.domain.repository.TaskRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class TaskCategoryService {
+
+    private final TaskCategoryRepository taskCategoryRepository;
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskCategoryService(final TaskCategoryRepository taskCategoryRepository,
+                               final TaskRepository taskRepository) {
+        this.taskCategoryRepository = taskCategoryRepository;
+        this.taskRepository = taskRepository;
+    }
+
+    @Transactional
+    public void deleteCategoryById(final Long taskCategoryId) {
+        final TaskCategory taskCategory = taskCategoryRepository.findById(taskCategoryId)
+                .orElseThrow(ResourceNotFoundException::new);
+
+        final List<Task> allTasksByUserAndCategory =
+                taskRepository.findAllByAuthorAndCategory(taskCategory.getUser(), taskCategory);
+        taskRepository.deleteAll(allTasksByUserAndCategory);
+        taskCategoryRepository.delete(taskCategory);
+    }
+
+}
