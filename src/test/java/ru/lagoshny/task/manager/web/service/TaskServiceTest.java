64875c4221693a3ae51524e68f50edbc99207fb3
ilya <ilya@lagoshny.ru>
2019-11-16 17:15:45 +0300

    Added TaskServiceTest

---
+++ src/test/java/ru/lagoshny/task/manager/web/service/TaskServiceTest.java
@@ -0,0 +1,180 @@
+package ru.lagoshny.task.manager.web.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import ru.lagoshny.task.manager.domain.entity.Task;
+import ru.lagoshny.task.manager.domain.entity.TaskCategory;
+import ru.lagoshny.task.manager.domain.entity.User;
+import ru.lagoshny.task.manager.domain.entity.enums.TaskStatusEnum;
+import ru.lagoshny.task.manager.domain.repository.TaskCategoryRepository;
+import ru.lagoshny.task.manager.domain.repository.TaskRepository;
+import ru.lagoshny.task.manager.utils.MockitoUtils;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class TaskServiceTest {
+
+    @InjectMocks
+    private TaskService taskService;
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private TaskCategoryRepository taskCategoryRepository;
+
+    @Test
+    public void shouldSetDefaultCategoryWhenCategoryIsNull() {
+        // GIVEN
+        final User author = new User();
+        final Task task = new Task();
+        task.setAuthor(author);
+        task.setCategory(null);
+        final TaskCategory defaultTaskCategory = TaskCategory.getDefault();
+
+        when(taskCategoryRepository.findByPrefixIgnoreCaseAndUser(defaultTaskCategory.getPrefix(), author))
+                .thenReturn(defaultTaskCategory);
+        when(taskRepository.save(task)).thenAnswer(MockitoUtils::returnFirstParam);
+
+        // WHEN
+        final TaskCategory actualTaskCategory = taskService.createTask(task).getCategory();
+
+        // THEN
+        assertThat(actualTaskCategory, equalTo(defaultTaskCategory));
+    }
+
+    @Test
+    public void shouldCreateDefaultCategoryForUserWhenHeHasNotOne() {
+        // GIVEN
+        final User author = new User();
+        final Task task = new Task();
+        task.setAuthor(author);
+        task.setCategory(null);
+
+        final TaskCategory defaultTaskCategory = TaskCategory.getDefault();
+        defaultTaskCategory.setUser(author);
+
+        when(taskCategoryRepository.findByPrefixIgnoreCaseAndUser(anyString(), eq(author)))
+                .thenReturn(null);
+        when(taskCategoryRepository.save(defaultTaskCategory)).thenAnswer(MockitoUtils::returnFirstParam);
+        when(taskRepository.save(task)).thenAnswer(MockitoUtils::returnFirstParam);
+
+        // WHEN
+        taskService.createTask(task);
+
+        // THEN
+        verify(taskCategoryRepository, times(1)).save(defaultTaskCategory);
+    }
+
+    @Test
+    public void taskNumberShouldBeIncreasedWhenCreateNewOneInSameCategory() {
+        // GIVEN
+        final TaskCategory taskCategory = TaskCategory.getDefault();
+        final Task task = new Task();
+        task.setCategory(taskCategory);
+
+        when(taskRepository.countByAuthorAndCategory(any(), eq(taskCategory))).thenReturn(0L);
+        when(taskRepository.save(task)).thenAnswer(MockitoUtils::returnFirstParam);
+
+        // WHEN
+        final Task createdTask = taskService.createTask(task);
+
+        // THEN
+        assertThat(createdTask.getNumber(), is(notNullValue()));
+        assertThat(createdTask.getNumber(), is(1L));
+    }
+
+    @Test
+    public void taskStatusShouldBeNewWhenCreateNewOne() {
+        // GIVEN
+        final TaskCategory taskCategory = TaskCategory.getDefault();
+        final Task task = new Task();
+        task.setCategory(taskCategory);
+
+        when(taskRepository.countByAuthorAndCategory(any(), eq(taskCategory))).thenReturn(0L);
+        when(taskRepository.save(task)).thenAnswer(MockitoUtils::returnFirstParam);
+
+        // WHEN
+        final Task createdTask = taskService.createTask(task);
+
+        // THEN
+        assertThat(createdTask.getStatus(), is(notNullValue()));
+        assertThat(createdTask.getStatus(), is(TaskStatusEnum.NEW));
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void shouldThrowExceptionWhenTaskToUpdateNotFound() {
+        // GIVEN
+        final Long nonExistTaskId = 0L;
+        final Task newTask = new Task();
+        when(taskRepository.findById(nonExistTaskId)).thenReturn(Optional.empty());
+
+        // WHEN
+        taskService.updateTask(nonExistTaskId, newTask);
+    }
+
+    @Test
+    public void taskNumberShouldBeUpdatedWhenNewTaskHasAnotherCategory() {
+        // GIVEN
+        final Task newTask = new Task();
+        final Long newCategoryCountTasks = 1L;
+        final TaskCategory newTaskCategory = new TaskCategory();
+        newTaskCategory.setName("New Category");
+        newTask.setCategory(newTaskCategory);
+        newTask.setNumber(null);
+
+        final Task existTask = new Task();
+        final TaskCategory existTaskCategory = new TaskCategory();
+        existTaskCategory.setName("Current Category");
+        existTask.setId(1L);
+        existTask.setCategory(existTaskCategory);
+        existTask.setNumber(1L);
+
+        when(taskRepository.findById(any())).thenReturn(Optional.of(existTask));
+        when(taskRepository.countByAuthorAndCategory(any(), eq(newTaskCategory)))
+                .thenReturn(newCategoryCountTasks);
+        when(taskRepository.save(newTask)).thenAnswer(MockitoUtils::returnFirstParam);
+
+        // WHEN
+        final Task updatedTask = taskService.updateTask(existTask.getId(), newTask);
+
+        // THEN
+        assertThat(updatedTask.getNumber(), is(newCategoryCountTasks + 1));
+    }
+
+    @Test
+    public void taskNumberShouldNotUpdateWhenUpdateTaskWithinSameCategory() {
+        // GIVEN
+        final TaskCategory taskCategory = new TaskCategory();
+        taskCategory.setName("Category");
+
+        final Task newTask = new Task();
+        newTask.setCategory(taskCategory);
+        newTask.setNumber(1L);
+
+        final Task existTask = new Task();
+        existTask.setCategory(taskCategory);
+        existTask.setNumber(1L);
+
+        when(taskRepository.findById(any())).thenReturn(Optional.of(existTask));
+        when(taskRepository.save(newTask)).thenAnswer(MockitoUtils::returnFirstParam);
+
+        // WHEN
+        final Task updatedTask = taskService.updateTask(1L, newTask);
+
+        // THEN
+        assertThat(updatedTask.getNumber(), is(newTask.getNumber()));
+    }
+
+}
