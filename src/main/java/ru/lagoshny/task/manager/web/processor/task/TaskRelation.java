9b0500451831be2e67ccdda0c8ec2a4aff19db9b
ilya <ilya@lagoshny.ru>
2020-02-16 22:06:31 +0300

    Added task relation constants

---
+++ src/main/java/ru/lagoshny/task/manager/web/processor/task/TaskRelation.java
@@ -0,0 +1,11 @@
+package ru.lagoshny.task.manager.web.processor.task;
+
+
+import ru.lagoshny.task.manager.domain.entity.Task;
+
+/**
+ * Contains all custom relations names for {@link Task} resource.
+ */
+public interface TaskRelation {
+   String UPDATE_STATUS_REL = "updateStatus";
+}
