453850a74cf95902086fc9e39475c42f6db87b19
ilya <ilya@lagoshny.ru>
2020-03-21 16:18:39 +0300

    Added user controller with service to create users with USER_ROLE

---
+++ src/main/java/ru/lagoshny/task/manager/web/service/UserService.java
@@ -0,0 +1,40 @@
+package ru.lagoshny.task.manager.web.service;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.lagoshny.task.manager.domain.entity.User;
+import ru.lagoshny.task.manager.domain.entity.UserRole;
+import ru.lagoshny.task.manager.domain.entity.enums.UserRoleEnum;
+import ru.lagoshny.task.manager.domain.repository.UserRepository;
+import ru.lagoshny.task.manager.domain.repository.UserRoleRepository;
+
+@Service
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+
+    @Autowired
+    public UserService(final PasswordEncoder passwordEncoder,
+                       final UserRepository userRepository,
+                       final UserRoleRepository userRoleRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
+    }
+
+    @Transactional
+    public User saveUser(@NotNull final User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        final User savedUser = userRepository.save(user);
+        userRoleRepository.save(new UserRole(savedUser, UserRoleEnum.ROLE_USER));
+
+        return savedUser;
+    }
+
+}
