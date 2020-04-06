76d37d7cd4f70066cea400c7674fab2c4975947b
ilya <ilya@lagoshny.ru>
2020-04-06 22:03:41 +0300

    Added new validator

---
+++ src/main/java/ru/lagoshny/task/manager/domain/validator/LatinWithNumbersAnd.java
@@ -0,0 +1,31 @@
+package ru.lagoshny.task.manager.domain.validator;
+
+import ru.lagoshny.task.manager.domain.validator.impl.LatinWithNumbersAndValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Validator description {@link LatinWithNumbersAndValidator}.
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {LatinWithNumbersAndValidator.class})
+public @interface LatinWithNumbersAnd {
+
+    String message() default "{combined.validation.latinWithNumberAnd.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String[] additionalSymbols() default {};
+
+}
