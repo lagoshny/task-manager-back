97cdb31e97b734320e57fa9fad2a23d0e4af3af2
Ilya Lagoshny <ilya@lagoshny.ru>
2025-11-29 23:19:30 +0300

    Migrate to Spring 4.0.0

--- src/test/java/ru/lagoshny/task/manager/config/TestSpringCommonConfig.java
+++ src/test/java/ru/lagoshny/task/manager/config/TestSpringCommonConfig.java
@@ -2,8 +2,6 @@ package ru.lagoshny.task.manager.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +11,7 @@ public class TestSpringCommonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
-        mapper.registerModule(new JavaTimeModule());
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
 
         return mapper;
     }
