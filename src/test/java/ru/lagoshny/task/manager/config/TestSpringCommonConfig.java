25df8feeb628d8859dd7c65202b55e4f7c7ee508
Ilya Lagoshny <ilya@lagoshny.ru>
2025-12-25 23:11:09 +0300

    Updated rest assured

--- src/test/java/ru/lagoshny/task/manager/config/TestSpringCommonConfig.java
+++ src/test/java/ru/lagoshny/task/manager/config/TestSpringCommonConfig.java
@@ -2,6 +2,7 @@ package ru.lagoshny.task.manager.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class TestSpringCommonConfig {
     public ObjectMapper objectMapper() {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL);
+        mapper.registerModule(new JavaTimeModule());
 
         return mapper;
     }
