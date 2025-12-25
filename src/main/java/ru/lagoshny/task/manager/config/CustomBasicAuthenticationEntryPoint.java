90b3b5efe347c526b144d18e531446a979fdc59e
Ilya Lagoshny <ilya@lagoshny.ru>
2025-12-25 23:24:03 +0300

    Upd imports

--- src/main/java/ru/lagoshny/task/manager/config/CustomBasicAuthenticationEntryPoint.java
+++ src/main/java/ru/lagoshny/task/manager/config/CustomBasicAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package ru.lagoshny.task.manager.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +11,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import ru.lagoshny.task.manager.web.exception.ServerError;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
